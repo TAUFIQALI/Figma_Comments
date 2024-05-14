f5acb8a48de42b2e624ffb183090e57ed2121e8d
TAUFIQALI <163372498+TAUFIQALI@users.noreply.github.com>
2024-05-14 13:34:03 +0600

    Update webhook-server.js

--- webhook-server.js
+++ webhook-server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 //add token ........
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.use(express.json());
 app.use(bodyParser.json());
 app.get('/api/test', (req, res) => {
 
