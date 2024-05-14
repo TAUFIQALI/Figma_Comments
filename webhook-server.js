261f6be0e3f7abfc4c21e2f6f422b464160857bb
TAUFIQALI <163372498+TAUFIQALI@users.noreply.github.com>
2024-05-14 12:18:09 +0600

    Update webhook-server.js

--- webhook-server.js
+++ webhook-server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { default: axios } = require('axios');
+//const { default: axios } = require('axios');
 //add token ........
 const app = express();
 const PORT = process.env.PORT || 3000;
